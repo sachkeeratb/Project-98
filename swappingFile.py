167e1cb8215ee0c57f884993cb318cca07ae1a18
vwxwv1 <61165141+vwxwv1@users.noreply.github.com>
2021-09-24 21:59:28 -0400

    Update swappingFile.py

--- swappingFile.py
+++ swappingFile.py
@@ -1,12 +1,14 @@
-file1 = input("What is file 1?")
-file2 = input("What is file 2?")
+file1 = input("Enter file 1")
+file2 = input("Enter file 2")
 
 def swapFileData(): 
-    with open("file1","r") as a:
+    with open(file1,"r") as a:
         data_a = a.read()
-    with open("file2","r") as b:
+    with open(file2,"r") as b:
         data_b = b.read()
-    with open("file1","w") as a:
+    with open(file1,"w") as a:
         a.write(data_b)
-    with open("file2","w") as b:
+    with open(file2,"w") as b:
         b.write(data_a)
+
+swapFileData()
